031ca0187b725b78c8116ffa0520dbf9b786bf79
unknown <nobakht9010@yahoo.com>
2023-01-03 16:04:02 +0330

    try to use mongoose models

--- index.js
+++ index.js
@@ -1,55 +1,84 @@
 const mongoClient = require("mongodb").MongoClient;
 const mongoUrl = require("./config/keys").mongoUrl;
+
+const mongoose = require("mongoose");
 const testDBName = "Test_decoding";
 const testResultDBName = "testResult";
-async function main(){
-    try{
-        const client = await mongoClient.connect(mongoUrl);// await is necessary 
-        console.log("connected to db")
-        const db = client.db(testDBName);
-        const collectionMain1 = db.collection("main_file_1");
-        const collectionMain2 = db.collection("main_file_2");
-        const collectionMain3 = db.collection("main_file_3");
+const testResultUrl = mongoUrl + "/" + testResultDBName;
+console.log(testResultUrl)
 
-        const collectionBHN1 = db.collection("CHR1_Bhn");
-        const collectionBHN2 = db.collection("CHR2_Bhn");
-        const collectionBHN3 = db.collection("CHR3_Bhn");
+const diffrenceModel = require("./models/diffrence");
 
-        const outs = await collectionMain2.aggregate([
-            {$project:{
-                'Procedure Messages':1,
-                _id:0
-            }}
-        ]).toArray()
+mongoose.set("strictQuery", false);
 
-        // console.log(outs);
+async function main() {
+  try {
+    mongoose
+      .connect(testResultUrl)
+      .then(() => console.log("mongoose connected to testResutlDb"))
+      .catch((err) => console.error(err));
+    const client = await mongoClient.connect(mongoUrl); // await is necessary
+    console.log("connected to db");
+    const db = client.db(testDBName);
+    const collectionMain1 = db.collection("main_file_1");
+    const collectionMain2 = db.collection("main_file_2");
+    const collectionMain3 = db.collection("main_file_3");
 
-        const tmp1 = await collectionMain2.findOne();
-        const keys = Object.keys(tmp1);
-        // console.log(keys);
-        // console.log(tmp1[keys[0]]);
+    const collectionBHN1 = db.collection("CHR1_Bhn");
+    const collectionBHN2 = db.collection("CHR2_Bhn");
+    const collectionBHN3 = db.collection("CHR3_Bhn");
 
-        for(let i=0; i < outs.length; i++){
-            const tmp1 = await collectionMain2.find({'Procedure Messages': outs[i]['Procedure Messages']}).toArray();
-            const tmp2 = await collectionBHN2.find({'Procedure Messages': outs[i]['Procedure Messages']}).toArray();
+    const outs = await collectionMain2
+      .aggregate([
+        {
+          $project: {
+            "Procedure Messages": 1,
+            _id: 0,
+          },
+        },
+      ])
+      .toArray();
 
-            for(let j=3; j <keys.length; j++){
-                if(tmp1[keys[j]] !== tmp2[keys[j]]){
+    // console.log(outs);
 
-                }
+    const tmp1 = await collectionMain2.findOne();
+    const keys = Object.keys(tmp1);
+    // console.log(keys);
+    console.log(tmp1[keys[3]]);
 
-            }
+    // for(let i=0; i < outs.length; i++){
+    // for (let i = 0; i < 1; i++) {
+    //   const tmp1 = await collectionMain2.findOne({
+    //     "Procedure Messages": outs[i]["Procedure Messages"],
+    //   });
+    //   const tmp2 = await collectionBHN2.findOne({
+    //     "Procedure Messages": outs[i]["Procedure Messages"],
+    //   });
 
+    //   // console.log(tmp1)
+    //   // console.log(tmp2)
+    //   if (tmp1 && tmp2) {
+    //     for (let j = 3; j < keys.length; j++) {
+    //     //   console.log(j);
+    //       if (tmp1[keys[j]] !== tmp2[keys[j]]) {
+    //         // console.log(tmp1[keys[j]]);
+    //         // console.log(tmp2[keys[j]]);
+    //       }
+    //     }
+    //   } else {
+    //     if (tmp1) {
+    //       console.log(i + "   " + "tmp2 isnt availabe");
+    //     } else {
+    //       console.log(i + "   " + "tmp1 isnt availabe");
+    //     }
+    //   }
+    // }
 
-            
-        }
-
-        await client.close()
-    }
-    catch(err){
-        console.error(err);
-        client.close()
-    }
+    await client.close();
+  } catch (err) {
+    console.error(err);
+    client.close();
+  }
 }
 
 main();

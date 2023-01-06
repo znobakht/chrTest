af89d9582e6a56108de68c083927e9b7803e1c4e
unknown <nobakht9010@yahoo.com>
2023-01-06 10:12:51 +0330

    index file is only for files number2

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const diffrenceModel = require("./models/diffrence");
 
 // mongoose.set("strictQuery", false);
 
+let client;
 async function main() {
   try {
     // mongoose
@@ -18,19 +19,19 @@ async function main() {
     //   .then(() => console.log("mongoose connected to testResutlDb"))
     //   .catch((err) => console.error(err));
 
-    const client = await mongoClient.connect(mongoUrl); // await is necessary
+    client = await mongoClient.connect(mongoUrl); // await is necessary
     console.log("connected to db");
 
     const db = client.db(testDBName);
     const testDB = client.db(testResultDBName);
 
-    const collectionMain1 = db.collection("main_file_1");
+    // const collectionMain1 = db.collection("main_file_1");
     const collectionMain2 = db.collection("main_file_2");
-    const collectionMain3 = db.collection("main_file_3");
+    // const collectionMain3 = db.collection("main_file_3");
 
-    const collectionBHN1 = db.collection("CHR1_Bhn");
+    // const collectionBHN1 = db.collection("CHR1_Bhn");
     const collectionBHN2 = db.collection("CHR2_Bhn");
-    const collectionBHN3 = db.collection("CHR3_Bhn");
+    // const collectionBHN3 = db.collection("CHR3_Bhn");
 
     const diffrenceCollection = testDB.collection("diffrences");
 
@@ -52,6 +53,7 @@ async function main() {
     // console.log(keys);
     // console.log(tmp1[keys[3]]);
 
+    console.log(outs.length);
     for(let i=0; i < outs.length; i++){
     // for (let i = 0; i < 1; i++) {
       console.log(i)

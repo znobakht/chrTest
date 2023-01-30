95d51c76e30c736da09d0ef71c555e51deb41a2e
zakie nobakht <zakie.nobakht@gmail.com>
2023-01-30 15:35:57 +0330

    logger added

--- printTime.js
+++ printTime.js
@@ -1,5 +1,8 @@
+import logger from "./logger.js";
 export const printTime = () => {
   let date = new Date();
   console.log(date.getHours());
+  logger.info(date.getHours());
   console.log(date.getMinutes());
+  logger.info(date.getMinutes())
 };

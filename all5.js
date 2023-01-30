7968a3d396ea18988b53dc39f3d876f2365464d0
zakie nobakht <zakie.nobakht@gmail.com>
2023-01-30 14:56:47 +0330

    for remain number of 5

---
+++ all5.js
@@ -0,0 +1,97 @@
+import { MongoClient } from "mongodb";
+import {
+  mongoUrl,
+  srcDBName,
+  dstDBName,
+  mainColTmp,
+  BHNColTmp,
+  startNumber,
+  endNumber,
+} from "./config/keys.js";
+
+import { printTime } from "./printTime.js";
+
+let client;
+const main = async () => {
+  // async function main() {
+  try {
+    client = await MongoClient.connect(mongoUrl); // await is necessary
+    console.log("connected to db");
+
+    const db = client.db(srcDBName);
+    const dstDB = client.db(dstDBName);
+
+    // for (let i = startNumber + 4; i <= endNumber; i++) {
+    for (let i = 5; i <= 5; i++) {
+      console.log(i);
+      printTime();
+
+      const srcCollName = `${mainColTmp}${i}`;
+
+      const collectionMain = db.collection(srcCollName);
+      const collectionBHN = db.collection(`${BHNColTmp}${i}`);
+
+      const differencesCollection = dstDB.collection("differences");
+
+      const PMIDS = await collectionMain
+        .aggregate([
+          {
+            $project: {
+              "Procedure Messages": 1,
+              _id: 0,
+            },
+          },
+        ])
+        .toArray();
+
+      const tmp1 = await collectionMain.findOne();
+      let keys = Object.keys(tmp1);
+
+      const unwantedKeys = ["_id", "DeviceName", "StartTime", "DateTime"];
+      keys = keys.filter((ele) => !unwantedKeys.includes(ele));
+
+      console.log(PMIDS.length);
+
+      // for (let j = 0; j < PMIDS.length; j++) {
+      for (let j = 13683; j < PMIDS.length; j++) {
+        // console.log(j);
+        const PMID = PMIDS[j]["Procedure Messages"];
+        console.log(PMID)
+
+        const tmp1 = await collectionMain.findOne({
+          "Procedure Messages": PMID,
+        });
+        const tmp2 = await collectionBHN.findOne({
+          "Procedure Messages": PMID,
+        });
+
+        if (tmp1 && tmp2) {
+          for (let k = 0; k < keys.length; k++) {
+            if (tmp1[keys[k]] !== tmp2[keys[k]]) {
+              await differencesCollection.insertOne({
+                procedureMessage: PMID,
+                fieldName: keys[k],
+                mainValue: tmp1[keys[k]],
+                bhnValue: tmp2[keys[k]],
+                fileNumber: srcCollName,
+              });
+            }
+          }
+        } else {
+          if (tmp1) {
+            console.log(j + "   " + "tmp2 isn't available");
+          } else {
+            console.log(j + "   " + "tmp1 isn't available");
+          }
+        }
+      }
+    }
+
+    await client.close();
+  } catch (err) {
+    console.error(err);
+    await client.close();
+  }
+};
+
+main();

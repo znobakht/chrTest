f522fa80fb93b1b94dfa6a5c9114992c5a705170
zakie nobakht <zakie.nobakht@gmail.com>
2023-01-30 10:09:01 +0330

    changed to es6 format

--- deleteAFileNumberRecords.js
+++ deleteAFileNumberRecords.js
@@ -1,22 +1,21 @@
-const mongoClient = require("mongodb").MongoClient;
-const mongoUrl = require("./config/keys").mongoUrl;
-const dbName = "testResult";
-const collectionName = "diffrences";
+import { MongoClient } from "mongodb";
+import { mongoUrl, dstDBName } from "./config/keys.js";
+const collectionName = "differences";
 
 let client;
 
-async function main(){
-    try{
-        client = await mongoClient.connect(mongoUrl);
-        console.log("connected to db");
-        let collection = client.db(dbName).collection(collectionName);
-        await collection.deleteMany({fileNumber:3});
-        client.close()
-    }
-    catch(err){
-        console.error(err);
-        await client.close();
-    }
+async function main() {
+  try {
+    client = await MongoClient.connect(mongoUrl);
+    console.log("connected to db");
+    let collection = client.db(dstDBName).collection(collectionName);
+    await collection.deleteMany({ fileNumber: "main_file_huwavei_CHR3" });
+    console.log("they have been deleted completely")
+    client.close();
+  } catch (err) {
+    console.error(err);
+    await client.close();
+  }
 }
 
-main()
+main();
